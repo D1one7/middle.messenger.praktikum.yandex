f18e751c3ab8949fcd11ef80d91494e2da1a1908
d1one7 <d1one7@ya.ru>
2023-02-21 00:39:10 +0300

    Fixes after review

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./dist/index.html"));
 app.listen(PORT, () => { console.log(`Server has been started on port ${PORT}`); });
